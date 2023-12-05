1174d693ea36ef1afe945fc98728cd4391d6f616
d1nhnguyen <dinhnguyen7205@gmail.com>
2023-12-05 14:45:55 +0700

    Update minigame.py

--- game/minigame.py
+++ game/minigame.py
@@ -15,7 +15,7 @@ def run():
             player_jump = pygame.image.load('assets/graphics/Player/jump.png').convert_alpha()
             self.player_jump = pygame.transform.rotozoom(player_jump,0,1.6)
             self.image = self.player_walk[self.player_index]
-            self.rect = self.image.get_rect(midbottom=(120, 500))
+            self.rect = self.image.get_rect(midbottom=(240, 500))
             self.gravity = 0
             self.jump_sound = pygame.mixer.Sound('assets/sfx/jumpsound.mp3')
             self.jump_sound.set_volume(0.5)
@@ -24,12 +24,8 @@ def run():
         def player_input(self):
             keys = pygame.key.get_pressed()
             if (keys[pygame.K_SPACE] or keys[pygame.K_UP]) and self.rect.bottom >= 500:
-                self.gravity = -20
+                self.gravity = -25
                 self.jump_sound.play()
-            if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and self.rect.right <= 1280:
-                self.rect.x += 5
-            if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and self.rect.left >= 0:
-                self.rect.x -= 5
         def apply_gravity(self):
             self.gravity += 1
             self.rect.y += self.gravity
@@ -60,27 +56,29 @@ def run():
                 fly_1 = pygame.transform.rotozoom(fly_1,0,1.6)
                 fly_2 = pygame.transform.rotozoom(fly_2, 0, 1.6)
                 self.frames = [fly_1, fly_2]
-                y_pos = 250
+                self.y_pos = 300
             else:
                 snail_1 = pygame.image.load('assets/graphics/snail/snail1.png').convert_alpha()
                 snail_2 = pygame.image.load('assets/graphics/snail/snail2.png').convert_alpha()
                 snail_1 = pygame.transform.rotozoom(snail_1,0,1.6)
                 snail_2 = pygame.transform.rotozoom(snail_2, 0, 1.6)
                 self.frames = [snail_1, snail_2]
-                y_pos = 500
+                self.y_pos = 500
 
             self.animation_index = 0
             self.image = self.frames[self.animation_index]
-            self.rect = self.image.get_rect(midbottom=(randint(1350, 1400), y_pos))
+            self.rect = self.image.get_rect(midbottom= (1280, self.y_pos))
 
         def animation_state(self):
-            self.animation_index += 0.1
+            self.animation_index += 0.2
             if self.animation_index >= len(self.frames): self.animation_index = 0
             self.image = self.frames[int(self.animation_index)]
 
         def update(self):
             self.animation_state()
-            self.rect.x -= 6
+            if self.y_pos == 250:
+                self.rect.x -= 15
+            else : self.rect.x -= 11
             self.destroy()
 
         def destroy(self):
@@ -89,7 +87,7 @@ def run():
 
 
     def display_score():
-        current_time = int(pygame.time.get_ticks() / 150) - start_time
+        current_time = int(pygame.time.get_ticks() / 350) - start_time
         score_surf = test_font.render(f'Score: {current_time}', False, (179, 19, 18))
         score_rect = score_surf.get_rect(center=(640, 50))
         screen.blit(score_surf, score_rect)
@@ -105,10 +103,13 @@ def run():
 
 
     pygame.init()
-    
+
+    icon = pygame.image.load('./assets/icons/game-icon.png')
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption('Bet the best')
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
-    test_font = pygame.font.Font('assets/font/Pixeltype.ttf', 100)
+    test_font = pygame.font.Font('assets/font/Pixeltype.ttf', 80)
     game_active = False
     start_time = 0
     score = 0
@@ -136,21 +137,22 @@ def run():
 
     game_over = test_font.render('Game Over!', False, (111, 196, 169))
     game_over_rect = game_over.get_rect(center=(640, 80))
-    
+
     game_beat = test_font.render('You won!', False, (111, 196, 169))
     game_beat_rect = game_beat.get_rect(center=(640, 80))
 
-    game_message = test_font.render('Press arrow keys to move', False, (111, 196, 169))
-    game_message_rect = game_message.get_rect(midtop=(640, 530))
 
-    game_message2 = test_font.render('Press space to start, Esc twice to exit', False, (188, 122, 249))
-    game_message2_rect = game_message2.get_rect(midtop=(640, game_message_rect.y + game_message.get_height() + 20))
 
-    game_pass = test_font.render ("Returning to 'Bet the best' in 4 seconds...", False, (188, 122, 249))
-    game_pass_rect = game_pass.get_rect(midtop = (640,530))
+    game_message2 = test_font.render('Press "SPACE" to start, "ESC" twice to exit', False, (188, 122, 249))
+    game_message2_rect = game_message2.get_rect(midtop=(640, 550))
+
+    game_passed = test_font.render ("Returning to 'Bet the best' in 4 seconds...", False, (188, 122, 249))
+    game_passed_rect = game_passed.get_rect(midtop = (640,530))
     # Timer
     obstacle_timer = pygame.USEREVENT + 1
-    pygame.time.set_timer(obstacle_timer, 1500)
+
+
+    pygame.time.set_timer(obstacle_timer, 800)
 
     #Choi nhac
     pygame.mixer.music.load('assets/musics/Minigame.mp3')
@@ -181,7 +183,7 @@ def run():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         game_active = True
-                        start_time = int(pygame.time.get_ticks() / 150)
+                        start_time = int(pygame.time.get_ticks() / 350)
                     elif event.key == pygame.K_ESCAPE:
                         current_time = pygame.time.get_ticks()
                         if current_time - last_esc_time < 1000:  # Check if ESC was pressed within 1 second
@@ -222,7 +224,6 @@ def run():
 
             if score == 0:
                 screen.blit(game_name, game_name_rect)
-                screen.blit(game_message, game_message_rect)
                 screen.blit(game_message2,game_message2_rect)
             elif score == 100:
                 start_time = pygame.time.get_ticks()
@@ -234,7 +235,7 @@ def run():
                             sys.exit()
 
                     screen.blit(game_beat,game_beat_rect)
-                    screen.blit(game_pass, game_pass_rect)
+                    screen.blit(game_passed, game_passed_rect)
 
                     pygame.display.update()
 
